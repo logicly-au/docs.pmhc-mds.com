06b674f2c27d68a89c41be72fff505b9ca2a7a3e
Russell Jenkins <russellj@logicly.com.au>
2024-09-06 16:41:56 +1000

    Add sphinx colour contrast extension

--- conf.py
+++ conf.py
@@ -31,7 +31,7 @@ from os import environ
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx_rtd_theme_ext_color_contrast']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
